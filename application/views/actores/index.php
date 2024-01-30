13625d15382350091601a59998055b7a656f812f
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2024-01-30 17:51:49 -0600

    Se añadio un div arriba de la tabla con la clase head-table y en el se añadio el boton Agregar Actor y se le añadio color a la tabla como en peliculas

--- application/views/actores/index.php
+++ application/views/actores/index.php
@@ -1,21 +1,31 @@
+<?php
+    $tr = 0;
+?>
 <main>
     <div class="div-table">
+        <div class="head-table">
+            <button class="btn-Agregar">Agregar Actor</button>
+        </div>
         <table cellpadding="0" cellspacing="0" border="1">
             <thead>
                 <tr>
-                    <th class="tg-0lax">#</th>
+                    <th>#</th>
                     <th>Nombre</th>
                     <th>Fecha de Nacimiento</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach($actores as $key=>$value):?>
-                    <tr>
+                    <?php $tr = $tr + 1; ?>
+                    <?php if($tr % 2 == 0):?>    
+                        <tr style="background-color: #faf9f9;">
+                    <?php else:?>
+                        <tr>
+                    <?php endif;?>
                         <td><?php echo $value->id_actor;?></td>
                         <td><?php echo $value->nombre;?> <?php echo $value->materno;?></td>
                         <td><?php echo $value->fecha_nacimiento;?></td>
                     </tr>
-
                 <?php endforeach; ?>
             </tbody>
         </table>
