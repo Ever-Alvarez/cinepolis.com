91f0c7e0f3979c62ca85c37d435eb7d7399fc75d
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2024-01-31 14:55:11 -0600

    Se añadio una clase column-color-si-no que con tiene un background-color para los <tr>

--- application/views/productoras/index.php
+++ application/views/productoras/index.php
@@ -1,8 +1,14 @@
+<?php
+    $tr = 0;
+?>
 <main>
     <div class="div-table">
+        <div class="head-table">
+            <button class="btn-Agregar">Agregar Productora</button>
+        </div>
         <table cellpadding="0" cellspacing="0" border="1">
             <thead>
-                <tr>
+                <tr class="column-color-si-no">
                     <th class="th-id">#</th>
                     <th>Nombre</th>
                     <th>Telefono</th>
@@ -15,7 +21,12 @@
             </thead>
             <tbody>
                 <?php foreach($productoras as $key=>$value): ?>
-                    <tr>
+                    <?php $tr = $tr + 1; ?>
+                    <?php if($tr % 2 == 0):?>    
+                        <tr class="column-color-si-no">
+                    <?php else:?>
+                        <tr>
+                    <?php endif;?>
                         <td><?php echo $value->id_productora; ?></td>
                         <td><?php echo $value->nombre; ?></td>
                         <td><?php echo $value->telefono; ?></td>
