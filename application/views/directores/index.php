c2346a179697af61bb5850a0b00e24e4394aa8a7
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2024-02-01 10:57:06 -0600

    Se añadio el boton Agregar Director junto con color en una columna y en otra no

--- application/views/directores/index.php
+++ application/views/directores/index.php
@@ -1,15 +1,26 @@
+<?php
+    $tr = 0;
+?>
 <main>
     <div class="div-table">
+        <div class="head-table">
+            <button class="btn-Agregar">Agregar Director</button>
+        </div>
         <table cellpadding="0" cellspacing="0" border="1" align="center">
             <thead>
-                <tr>
+                <tr class="column-color-si-no">
                     <th>#</th>
                     <th>Nombre</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach($directores as $key=>$value):?>
-                    <tr>
+                    <?php $tr = $tr + 1; ?>
+                    <?php if($tr % 2 == 0):?>    
+                        <tr class="column-color-si-no">
+                    <?php else:?>
+                        <tr>
+                    <?php endif;?>
                         <td><?php echo $value->id_director;?></td>
                         <td><?php echo $value->nombre.' '.$value->paterno;?></td>
                     </tr>
