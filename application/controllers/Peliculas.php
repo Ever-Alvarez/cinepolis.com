3af890f98aaf0b322ff96f8660d8cbaf6b338b3e
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2024-02-06 16:21:04 -0600

    Se creo el controlador Peliculas con la funcion index que muestra la vista de peliculas/index

---
+++ application/controllers/Peliculas.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Peliculas extends CI_Controller{
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+
+        $this->load->model('Cinepolis_model');
+        $peliculas = $this->Cinepolis_model->listaPeliculas();
+        
+        $data = [
+            'title' => 'Cinépolis - Peliculas',
+            'peliculas' => $peliculas
+        ];
+
+        //echo"<pre>";
+        //print_r($peliculas);
+        //echo"</pre>";
+        
+        $this->load->view('plantillas/header', $data);
+        $this->load->view('peliculas/index', $data);
+        // $this->load->view('plantillas/footer');
+
+    }
+
+}
