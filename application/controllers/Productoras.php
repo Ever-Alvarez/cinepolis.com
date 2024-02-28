17d0d17a844848fbf91db7177fd7bd547ec8473b
Ever-Alvarez <ever.garzaalvarez25@gmail.com>
2024-02-28 12:50:45 -0600

    Correcion

--- application/controllers/Productoras.php
+++ application/controllers/Productoras.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Productoras extends CI_Controller {
 
     public function __construct() {
-        parents::__construct();
+        parent::__construct();
     }
 
     public function index() {
